f70d4506b7f3361cb9d0d1c7b508675ce350b068
Abdul-samad-12345 <146140053+Abdul-samad-12345@users.noreply.github.com>
2023-12-06 02:30:43 +0530

    Create 54.js

---
+++ 54.js
@@ -0,0 +1,17 @@
+// map,filter, reduce
+const arr = [1, 2, 3, 4, 5];
+
+// const res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     res.push(arr[i] * 2);
+// }
+
+
+function times3(elem) {
+    return elem * 3;
+}
+
+const res = arr.map(elem => elem * 2);
+const res2 = arr.map(elem => elem * 3);
+
+console.log(arr, res, res2);
